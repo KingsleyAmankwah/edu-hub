4f507b3d45a6e1204e098b6e886f1c0ff55ce2af
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-07-16 04:28:53 +0000

    firebase-config file

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7-Thb2SWYoU36lbIsOuyki4n7KCmNJvA",
+  authDomain: "edu-hub-199d6.firebaseapp.com",
+  projectId: "edu-hub-199d6",
+  storageBucket: "edu-hub-199d6.appspot.com",
+  messagingSenderId: "173867802787",
+  appId: "1:173867802787:web:a9ea8b1790515b88611f16",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
