2745e158ab78489d793927824ea7d8164bc1dd4d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-07-17 17:17:43 +0000

    Login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -55,7 +55,7 @@ function Login({ closeModal }) {
          flex flex-col  justify-center h-[70%] bg-white  z-50 rounded-2xl"
       >
         <div
-          className="absolute right-[10px] top-[10px] bg-primary cursor-pointer text-black text-xl py-1 px-4 rounded-lg"
+          className="absolute right-[10px] top-[10px] bg-primary cursor-pointer text-white bg-[#f50057] text-xl py-1 px-4 rounded-lg"
           onClick={() => closeModal(false)}
         >
           X
