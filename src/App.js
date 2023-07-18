04107e9d706654183d1d89862e59dc89f7708232
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-07-18 04:43:28 +0000

    App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 // import Header from "./components/Header";
 // import Nav from "./components/Nav";
 import Profile from "./pages/profile";
 import Home from "./pages/Home";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
@@ -13,10 +16,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
 
-      {/* <ToastContainer /> */}
+      <ToastContainer />
     </div>
   );
 }
