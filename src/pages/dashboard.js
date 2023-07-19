d96685391e78c728d8a3f645bb6b55235fd69bba
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-07-19 04:40:04 +0000

    Dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,57 @@
+import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+function Dashboard() {
+  const [user, setUser] = useState(null);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const auth = getAuth();
 
-function dashboard() {
+  useEffect(() => {
+    const user = auth.currentUser;
+    setUser(user);
+  }, [auth.currentUser]);
+
+  const handleDateChange = (date) => {
+    setCurrentDate(date);
+  };
   return (
-    <div>
+    <div className="w-full h-full">
       <Nav />
+      <div className="h-[100vh] w-full">
+        <div className="flex flex-col md:flex-row w-full justify-around mt-8">
+          <div>
+            <h1 className="font-bold text-lg md:text-3xl pb-4">
+              Welcome {user && user?.displayName} 👋
+            </h1>
+
+            <Calendar value={currentDate} onChange={handleDateChange} />
+          </div>
+
+          <div>
+            <span className="text-primary font-bold">Join Class</span>
+            <div className="bg-[#f6f7f8] mt-4 rounded-lg p-8">
+              <Link to="/groups" className="text-center underline w-full">
+                Basic 7
+              </Link>
+            </div>
+            <div className="w-full bg-[#f6f7f8] mt-4 rounded-lg p-8">
+              <Link to="/groups" className="text-center underline">
+                Basic 8
+              </Link>
+            </div>
+            <div className="w-full bg-[#f6f7f8] mt-4 rounded-lg p-8">
+              <Link to="/groups" className="text-center underline">
+                Basic 9
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default dashboard;
+export default Dashboard;
