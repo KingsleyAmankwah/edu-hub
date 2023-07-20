6a7acfcb85293bcb0580e8781135e72e5cbd694e
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-07-20 03:48:30 +0000

    Forgot password

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,11 +1,40 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-// import Spinner from "../components/Spinner";
+import Spinner from "../components/Spinner";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
 
-  //   if (isLoading) return <Spinner />;
+  const [resetMessage, setResetMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [resetMessageBgColor, setResetMessageBgColor] = useState("bg-red-200");
+  //   let resetMessageBgColor = "bg-red-200";
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const auth = getAuth();
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setResetMessage(
+        "Password reset email sent. Check your email for further instructions."
+      );
+      setResetMessageBgColor("bg-green-200");
+    } catch (error) {
+      if (error.code === "auth/user-not-found") {
+        setResetMessage(
+          "Email not found in our system. Please make sure you entered the correct email address."
+        );
+      } else {
+        setResetMessage("An error occurred. Please try again later.");
+      }
+    }
+
+    setIsLoading(false);
+  };
+
+  if (isLoading) return <Spinner />;
 
   return (
     <>
@@ -15,6 +44,20 @@ function ForgotPassword() {
             <div className="w-full xl:w-3/4 lg:w-11/12 flex">
               <div className="w-full bg-white p-5 rounded-lg lg:rounded-l-none">
                 <div className="sm:px-8 mb-4 text-center">
+                  {resetMessage && (
+                    <div
+                      className={`mb-4 text-sm text-center rounded-lg p-2 ${resetMessageBgColor} text-red-700`}
+                    >
+                      {resetMessage}
+                      <button
+                        onClick={() => setResetMessage("")}
+                        className="ml-2 bg-[#e60023] text-white px-4 py-2 rounded-lg font-semibold focus:outline-none"
+                      >
+                        X
+                      </button>
+                    </div>
+                  )}
+
                   <h3 className="pt-4 mb-2 text-lg sm:text-2xl">
                     Forgot Your Password?
                   </h3>
@@ -23,9 +66,10 @@ function ForgotPassword() {
                     below and we'll send you a link to reset your password!
                   </p>
                 </div>
+
                 <form
                   className="sm:px-8 pt-6 pb-8 mb-4 bg-white rounded"
-                  //   onSubmit={onSubmit}
+                  onSubmit={handleResetPassword}
                 >
                   <div className="mb-4">
                     <label className="block mb-2 text-sm font-bold text-gray-700">
