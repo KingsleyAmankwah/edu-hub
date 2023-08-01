5b7a381ffc299b2f8dbfb3619b5b6a6554137e95
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-08-01 02:04:15 +0000

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
-
 import Logo from "../assets/logo.png";
-import Login from "./Login";
-import SignUp from "./SignUp";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [loginModal, setLoginModal] = useState(false);
-  const [signUpModal, setSignUpModal] = useState(false);
-
   return (
     <div className="py-2 px-4 h-[10vh] flex justify-between w-full items-center z-40 shadow-lg">
       <div className="flex justify-center items-center w-[20%] gap-5">
@@ -15,20 +10,17 @@ const Header = () => {
         <span className="font-[500] text-sm"> EduHub</span>
       </div>
 
-      <div className="flex items-center justify-evenly w-[10rem]">
-        <div className="cursor-pointer" onClick={() => setLoginModal(true)}>
-          Login
-        </div>
-        <div
-          className="rounded-[8px] cursor-pointer py-2 m-4 bg-[#f50057] px-4 text-white"
-          onClick={() => setSignUpModal(true)}
-        >
-          Sign Up
-        </div>
+      <div className="flex justify-around items-center w-[50%] text-sm">
+        <Link to="/seven" className="hover:text-[#f50057]">
+          Basic 7
+        </Link>
+        <Link to="/eight" className="hover:text-[#f50057]">
+          Basic 8
+        </Link>
+        <Link to="/nine" className="hover:text-[#f50057]">
+          Basic 9
+        </Link>
       </div>
-
-      {loginModal && <Login closeModal={setLoginModal} />}
-      {signUpModal && <SignUp closeModal={setSignUpModal} />}
     </div>
   );
 };
