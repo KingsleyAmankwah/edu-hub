ba8030d6dc388a67043aa1757af339f37ee60aed
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-08-09 01:50:38 +0000

    Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,14 +5,20 @@ const Nav = () => {
   return (
     <div className="py-2 px-4 h-[10vh] flex justify-between w-full items-center z-40 shadow-lg">
       <div className="flex justify-center items-center w-[20%] gap-5">
-        <img className="flex sm:w-8 sm:h-8" src={Logo} alt="logo" />
-        <span className="font-[500] hidden md:flex text-sm"> EduHub</span>
+        <img className="hidden md:flex" src={Logo} alt="EduHub_Logo" />
+        <span className="font-[500] text-sm"> EduHub</span>
       </div>
 
-      <div className="flex justify-around items-center w-[50%] text-sm">
-        <Link to="/">Home</Link>
-        <Link to="/">About Us</Link>
-        <Link to="/">Contact</Link>
+      <div className="flex justify-around items-center w-[50%] text-sm ">
+        <a href="/" className="hover:text-[#f50057]">
+          Home
+        </a>
+        <a href="#about" className="hover:text-[#f50057]">
+          About Us
+        </a>
+        <a href="#contact" className="hover:text-[#f50057]">
+          Contact
+        </a>
       </div>
       <div></div>
     </div>
